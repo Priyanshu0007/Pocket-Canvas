1665306ae675b4959a7c4c4c587ae5ec2c2ec11d
Priyanshu0007 <priyanshu2859@gmail.com>
2026-07-11 19:39:21 +0530

    Initial Commit

---
+++ Pocket Canvas/Pocket_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_CanvasApp.swift
+//  Pocket Canvas
+//
+//  Created by Priyanshu Gupta on 11/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
